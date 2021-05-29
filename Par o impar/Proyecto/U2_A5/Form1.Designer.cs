72d6aa65c6777e6805647b306e479fd3d0622e4b
tlalok-lab <66752848+tlalok-lab@users.noreply.github.com>
2021-05-29 16:50:06 -0500

    Add files via upload
    Primer commit

---
+++ Par o impar/Proyecto/U2_A5/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace U2_A5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.boton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // boton
+            // 
+            this.boton.BackColor = System.Drawing.Color.Blue;
+            this.boton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("boton.BackgroundImage")));
+            this.boton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton.Location = new System.Drawing.Point(463, 181);
+            this.boton.Name = "boton";
+            this.boton.Size = new System.Drawing.Size(298, 89);
+            this.boton.TabIndex = 0;
+            this.boton.Text = "Par o Impar";
+            this.boton.UseVisualStyleBackColor = false;
+            this.boton.Click += new System.EventHandler(this.boton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresa un número";
+            // 
+            // num
+            // 
+            this.num.BackColor = System.Drawing.Color.Azure;
+            this.num.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num.Location = new System.Drawing.Point(77, 194);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(195, 44);
+            this.num.TabIndex = 2;
+            this.num.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.num.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.num_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.BackColor = System.Drawing.Color.Transparent;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(228, 329);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(322, 73);
+            this.result.TabIndex = 4;
+            this.result.Text = "Resultado";
+            this.result.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(456, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(332, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Da click para verificar ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(806, 57);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Verificación de números pares o impares ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "U1_A5_LR_CF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button boton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Par o impar/Proyecto/U2_A5/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_A5
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void boton_Click(object sender, EventArgs e)
+        {
+            /*Declaracion de variables*/
+            double N1;
+            string R;
+            /*Asignacion de datos capturados  a la variable N1*/
+            N1 = double.Parse(num.Text);
+            {
+                /*Condicion para saber si el residuo es 0*/
+                if ((N1 % 2) == 0)
+                {
+                    /*Asignar cadena de datos a R*/
+                    R = "Número Par";
+                    /*imprime resultado en etiqueta result*/
+                    result.Text = R.ToString();                    
+                }
+                else
+                {
+                    /*Asignar cadena de datos a R*/
+                    R = "Número Impar";
+                    /*imprime resultado en etiqueta result*/
+                    result.Text = R.ToString();               
+                }
+            }
+            /*limpia la caja de texto que captura el numero a dividir*/
+            num.Text = "";
+        }
+
+         private void textBox1_TextChanged(object sender, EventArgs e)
+         {
+
+         }
+
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*validacion para solocapturar numeros y  mesaje en caso de ingresar otro tipo de caracteres*/
+            if ((e.KeyChar >= 32 && e.KeyChar <=47) || (e.KeyChar >= 58 && e.KeyChar <=255))
+            {
+                MessageBox.Show("Ingresa solo Números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+    }
+}
